1bcdb7dcace5946494f173c3600a5a858f94e1e1
Robin & Nathan <nathanhinchey@gmail.com>
2015-04-10 09:02:19 -0400

    finish polls project

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,98 @@ class User < ActiveRecord::Base
   )
 
 
+  # def completed_polls
+  #   sqlquery = <<-SQL
+  #
+  #   SELECT
+  #     polls.*
+  #     -- COUNT(questions.id) AS total_questions_count,
+  #     -- COUNT(user_responses.q_id) AS user_answers_count
+  #   FROM
+  #     polls
+  #   JOIN
+  #     questions
+  #   ON
+  #     polls.id = questions.poll_id
+  #   LEFT OUTER JOIN
+  #     (
+  #       SELECT
+  #         ac.question_id AS q_id
+  #       FROM
+  #         responses AS r
+  #       JOIN
+  #         users AS u
+  #       ON
+  #         u.id = r.user_id
+  #       JOIN
+  #         answer_choices AS ac
+  #       ON
+  #         ac.id = r.answer_choice_id
+  #       WHERE
+  #         u.id = ?
+  #     ) AS user_responses
+  #   ON
+  #     user_responses.q_id = questions.id
+  #   GROUP BY
+  #     polls.id
+  #   HAVING
+  #     COUNT(questions.id) > COUNT(user_responses.q_id)
+  #       AND COUNT(user_responses.q_id) > 0
+  #   SQL
+  #
+  #   Poll.find_by_sql([sqlquery, id])
+  # end
+
+  def completed_polls
+    sqlquery_without_having = <<-SQL
+
+    SELECT
+      polls.*
+      -- COUNT(questions.id) AS total_questions_count,
+      -- COUNT(user_responses.q_id) AS user_answers_count
+    FROM
+      polls
+    JOIN
+      questions
+    ON
+      polls.id = questions.poll_id
+    LEFT OUTER JOIN
+      (
+        SELECT
+          ac.question_id AS q_id
+        FROM
+          responses AS r
+        JOIN
+          users AS u
+        ON
+          u.id = r.user_id
+        JOIN
+          answer_choices AS ac
+        ON
+          ac.id = r.answer_choice_id
+        WHERE
+          u.id = ?
+      ) AS user_responses
+    ON
+      user_responses.q_id = questions.id
+    GROUP BY
+      polls.id
+    SQL
+
+    incomplete_having = <<-SQL
+    HAVING
+      COUNT(questions.id) > COUNT(user_responses.q_id)
+        AND COUNT(user_responses.q_id) > 0
+    SQL
+
+    complete_having = <<-SQL
+    HAVING
+      COUNT(questions.id) = COUNT(user_responses.q_id)
+    SQL
+
+
+    Poll.find_by_sql([sqlquery_without_having + complete_having, id])
+    #have to use real string addition because we DON'T want to santize inputs
+
+  end
 end
